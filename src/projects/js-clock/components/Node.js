3a7f9fbc57bb954a573e35ad1eaa06b332c032e5
hallovarvara <hallovarvara@gmail.com>
2022-12-22 23:35:58 +0000

    feat: add base for js-clock

---
+++ src/projects/js-clock/components/Node.js
@@ -0,0 +1,7 @@
+import { Node as NodeBase } from '../../../components/Node.js';
+
+export class Node extends NodeBase {
+  constructor(props) {
+    super(props);
+  }
+}
---
+++ src/projects/js-clock/index.js
@@ -0,0 +1,5 @@
+export class JSClock {
+  id = 'js-clock';
+
+  constructor() {}
+}
